2c00b784dfaca28bf72593777718436779265df0
Cédric Mariotti <cedricmariotti@gmail.com>
2025-06-08 10:29:01 +0200

    Update ReplaceXthCharacter-inName.lua

--- Utility/ReplaceXthCharacter-inName.lua
+++ Utility/ReplaceXthCharacter-inName.lua
@@ -1,15 +1,20 @@
--- @description CharacterText Interchange in Names
--- @author Mariow
--- @version 1.0
--- @changelog Initial Release
--- @provides
---   [main] Utility/ReplaceXthCharacter-inName.lua
--- @link https://github.com/Geeksound/Reaper_Scripts-Mariow
--- @tags name,items,editing
--- @about
---   # ReplaceXthCharacter-inName
---   Contextual Replacement of a Character of sequence of Character in Reaper 7.0.
---   This script was developed with the help of GitHub Copilot.
+--[[
+@description CharacterText Interchange in Names
+@version 1.0
+@author Mariow
+@changelog
+  v1.0 (2025-06-08)
+  - Initial Release
+@provides
+  [main] Utility/ReplaceXthCharacter-inName.lua
+@link https://github.com/Geeksound/Reaper_Scripts-Mariow
+@repository https://github.com/Geeksound/Reaper_Scripts-Mariow
+@tags name, items, editing
+@about
+  # ReplaceXthCharacter-inName
+  Contextual Replacement of a Character or sequence of Characters in Reaper 7.0.
+  This script was developed with the help of GitHub Copilot.
+--]]
 
 -- Ask the user for the text to replace
 local ret1, a_remplacer = reaper.GetUserInputs("Text Replacement", 1, "Text to Replace:", "")
