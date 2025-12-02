1c2013d0f8e780a6964824b801937d986bdef13c
Geeksound <cedricmariotti@gmail.com>
2025-12-02 13:51:07 +0100

    MAJ TRIM START TO PREV

--- Editing_TRIM/UnTrimStartToPrev.lua
+++ Editing_TRIM/UnTrimStartToPrev.lua
@@ -1,8 +1,10 @@
 --[[
 @description Trim Left Edge of Selected Item to Previous Item on Same Track (with left edge check)
-@version 1.1
+@version 1.1.1
 @author Mariow
 @changelog
+  V.1.1.1 (2025-12-02)
+  - Bug Fix
   v1.1 (2025-11-07)
   - Added condition: left edge will not move if already aligned with previous item's right edge
   v1.0 (2025-11-06)
@@ -21,37 +23,44 @@
 
 -- @noindex
 
+
 reaper.Undo_BeginBlock()
 reaper.PreventUIRefresh(1)
 
--- Récupérer le premier item sélectionné
 local item = reaper.GetSelectedMediaItem(0, 0)
 if item then
     local track = reaper.GetMediaItemTrack(item)
     local item_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-    
-    -- Chercher l'item précédent sur la même piste
+
+    -- Trouver le précédent item sur la même piste
     local item_count = reaper.CountTrackMediaItems(track)
     local prev_item = nil
     local prev_end = -math.huge
 
     for i = 0, item_count - 1 do
         local it = reaper.GetTrackMediaItem(track, i)
-        local it_end = reaper.GetMediaItemInfo_Value(it, "D_POSITION") + reaper.GetMediaItemInfo_Value(it, "D_LENGTH")
+        local it_pos = reaper.GetMediaItemInfo_Value(it, "D_POSITION")
+        local it_len = reaper.GetMediaItemInfo_Value(it, "D_LENGTH")
+        local it_end = it_pos + it_len
+
         if it_end <= item_pos and it_end > prev_end then
             prev_item = it
             prev_end = it_end
         end
     end
 
-    -- Condition : si le bord gauche est déjà aligné, ne rien faire
+    -- Vérifier qu’il y a un item précédent ET pas déjà aligné
     if prev_item and math.abs(item_pos - prev_end) > 0.0001 then
-        local item_end = item_pos + reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
-        reaper.SetMediaItemInfo_Value(item, "D_POSITION", prev_end)
-        reaper.SetMediaItemInfo_Value(item, "D_LENGTH", item_end - prev_end)
+        
+        -- 1. placer le curseur
+        reaper.SetEditCurPos(prev_end, false, false)
+        
+        -- 2. utiliser la commande native : Trim left edge to edit cursor
+        -- 41305 = "Item: Trim left edge of item to edit cursor"
+        reaper.Main_OnCommand(41305, 0)
     end
 end
 
 reaper.UpdateArrange()
 reaper.PreventUIRefresh(-1)
-reaper.Undo_EndBlock("Trim Left Edge to Previous Item (with check)", -1)
+reaper.Undo_EndBlock("Trim Left Edge to Previous Item (safe)", -1)
