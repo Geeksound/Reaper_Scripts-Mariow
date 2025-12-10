fbbf92eedea9ad1edcf0ab359738b01c23bd8283
Cédric Mariotti <cedricmariotti@gmail.com>
2025-12-10 10:59:18 +0100

    Fix file name in provides sectionProTools_Essentials/MoveTimeSelStartForward-TCui.lua

--- ProTools_Essentials/MoveTimeSelStartForward-TCui.lua
+++ ProTools_Essentials/MoveTimeSelStartForward-TCui.lua
@@ -6,7 +6,7 @@
     v1.0 (2025-12-09)
     - Initial release: moves the start of the Time Selection forward according to TimecodeUI nudge value
 @provides
-    [main] ProTools_Essentials/MoveTimeSelectionStartForward-TCui.lua
+    [main] ProTools_Essentials/MoveTimeSelStartForward-TCui.lua
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
 @tags editing, timesel, nudge, transport, forward, (protools-like)
