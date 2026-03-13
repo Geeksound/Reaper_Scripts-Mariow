c435195803eb8452a8daeb62a2d2bb72e286b9ba
Cédric Mariotti <cedricmariotti@gmail.com>
2026-03-13 21:27:14 +0100

    Update Geeksound_ViewFile-FieldRecorder-Metadatas.lua

--- Metadatas/Geeksound_ViewFile-FieldRecorder-Metadatas.lua
+++ Metadatas/Geeksound_ViewFile-FieldRecorder-Metadatas.lua
@@ -6,7 +6,7 @@
 @changelog
   v1.0 (2025-06-07)
   - Initial release
-@provides [main] Metadatas/ViewFile-FieldRecorder-Metadatas.lua
+@provides [main] Metadatas/Geeksound_ViewFile-FieldRecorder-Metadatas.lua
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
 @tags items metadata source properties
