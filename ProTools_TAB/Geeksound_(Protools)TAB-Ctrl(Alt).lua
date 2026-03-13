b27a58ec3986151ae9bc22725e31f7b2899fdfd4
Cédric Mariotti <cedricmariotti@gmail.com>
2026-03-14 00:05:35 +0100

    Update Geeksound_(Protools)TAB-Ctrl(Alt).lua

--- ProTools_TAB/Geeksound_(Protools)TAB-Ctrl(Alt).lua
+++ ProTools_TAB/Geeksound_(Protools)TAB-Ctrl(Alt).lua
@@ -6,7 +6,7 @@
 	v1.0 (2025-11-17)
 	- Initial release
 @provides
-	[main] ProTools_TAB/(ProTools)TAB-Ctrl(Alt).lua
+	[main] ProTools_TAB/Geeksound_(ProTools)TAB-Ctrl(Alt).lua
 @tags protools, tab, item, selection, editing, previous
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
