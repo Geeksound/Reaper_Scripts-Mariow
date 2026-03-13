78b4dafda06c2c057f6ebd6ec19e791b000388c8
Cédric Mariotti <cedricmariotti@gmail.com>
2026-03-13 16:42:47 +0100

    Update Geeksound_ReplaceXthCharacter-inName.lua

--- Utility/Geeksound_ReplaceXthCharacter-inName.lua
+++ Utility/Geeksound_ReplaceXthCharacter-inName.lua
@@ -6,7 +6,7 @@
   v1.0 (2025-06-08)
   - Initial Release
 @provides
-  [main] Utility/ReplaceXthCharacter-inName.lua
+  [main] Utility/Geeksound_ReplaceXthCharacter-inName.lua
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
 @tags name, items, editing
