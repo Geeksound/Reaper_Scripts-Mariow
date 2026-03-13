cd0d13e61eac5d498e16b7fbcc70c532fc6bf767
Cédric Mariotti <cedricmariotti@gmail.com>
2026-03-13 21:17:34 +0100

    Update Geeksound_Set-ItemFor-IXMLRendering.lua

--- Field-Recorder_Workflow/Geeksound_Set-ItemFor-IXMLRendering.lua
+++ Field-Recorder_Workflow/Geeksound_Set-ItemFor-IXMLRendering.lua
@@ -7,7 +7,7 @@
   v1.0 (2025-06-08)
   - Initial release
 @provides
-  [main] Field-Recorder_Workflow/Set-ItemFor-IXMLRendering.lua
+  [main] Field-Recorder_Workflow/Geeksound_Set-ItemFor-IXMLRendering.lua
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
 @tags dialogue conformation workflow Fieldrecording Track rushs raw files matching conforming
