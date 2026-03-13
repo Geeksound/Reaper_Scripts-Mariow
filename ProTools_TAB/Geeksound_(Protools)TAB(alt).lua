bee07668bcd028839d98142c2cfd8f7cbdb18424
Cédric Mariotti <cedricmariotti@gmail.com>
2026-03-14 00:02:43 +0100

    Update Geeksound_(Protools)TAB(alt).lua

--- ProTools_TAB/Geeksound_(Protools)TAB(alt).lua
+++ ProTools_TAB/Geeksound_(Protools)TAB(alt).lua
@@ -6,7 +6,7 @@
     v1.0 (2025-06-07)
     - Initial release
 @provides
-    [main] ProTools_TAB/(Protools)TAB(alt).lua
+    [main] ProTools_TAB/Geeksound_(Protools)TAB(alt).lua
 @tags protools, tab, reverse, navigation, transient, fade, editing, cursor
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
