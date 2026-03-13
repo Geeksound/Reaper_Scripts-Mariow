1dbef97a5323e77ee81912ef8085a52386827b24
Cédric Mariotti <cedricmariotti@gmail.com>
2026-03-14 00:10:30 +0100

    Update Geeksound_(Protools)TAB-TransientToggleShortcut.lua

--- ProTools_TAB/Geeksound_(Protools)TAB-TransientToggleShortcut.lua
+++ ProTools_TAB/Geeksound_(Protools)TAB-TransientToggleShortcut.lua
@@ -8,7 +8,7 @@
     v1.0 (2025-06-07)
     - Initial release
 @provides
-    [main] ProTools_TAB/(Protools)TAB-TransientToggleShortcut.lua
+    [main] ProTools_TAB/Geeksound_(Protools)TAB-TransientToggleShortcut.lua
 @tags protools, transient, tab, shortcut, editing
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
