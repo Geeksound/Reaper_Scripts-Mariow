d1f680a33bdfc235cd1ce7b1206827a415887c3e
Cédric Mariotti <cedricmariotti@gmail.com>
2026-03-13 20:28:52 +0100

    Update Geeksound_Swap-ItemsPositionsOnSameTracks.lua

--- Editing/Geeksound_Swap-ItemsPositionsOnSameTracks.lua
+++ Editing/Geeksound_Swap-ItemsPositionsOnSameTracks.lua
@@ -6,7 +6,7 @@
   v1.0 (2025-06-08)
   - Initial release
 @provides
-  [main] Editing/Swap-ItemsPositionsOnSameTracks.lua
+  [main] Editing/Geeksound_Swap-ItemsPositionsOnSameTracks.lua
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
 @tags items, editing
