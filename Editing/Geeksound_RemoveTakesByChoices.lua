29262b86672a47bf63d31974fe0130726af983ca
Cédric Mariotti <cedricmariotti@gmail.com>
2026-03-13 20:27:16 +0100

    Update Geeksound_RemoveTakesByChoices.lua

--- Editing/Geeksound_RemoveTakesByChoices.lua
+++ Editing/Geeksound_RemoveTakesByChoices.lua
@@ -6,7 +6,7 @@
   v1.0 (2025-11-09)
   - Initial release
 @provides
-  [main] Editing/RemoveTakesByChoices.lua
+  [main] Editing/Geeksound_RemoveTakesByChoices.lua
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
 @tags takes, media items, editing
