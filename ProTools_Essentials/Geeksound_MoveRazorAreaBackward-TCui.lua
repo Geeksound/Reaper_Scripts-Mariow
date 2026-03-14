e169a73418f3052fd448ad73894453cf786a127d
Cédric Mariotti <cedricmariotti@gmail.com>
2026-03-14 09:38:15 +0100

    Update and rename MoveRazorAreaBackward-TCui.lua to Geeksound_MoveRazorAreaBackward-TCui.lua

--- ProTools_Essentials/Geeksound_MoveRazorAreaBackward-TCui.lua
+++ ProTools_Essentials/Geeksound_MoveRazorAreaBackward-TCui.lua
@@ -6,7 +6,7 @@
     v1.0 (2025-12-09)
     - Initial release (moves all Razor Areas backward using TimecodeUI nudge value)
 @provides
-    [main] ProTools_Essentials/MoveRazorAreaBackward-TCui.lua
+    [main] ProTools_Essentials/Geeksound_MoveRazorAreaBackward-TCui.lua
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
 @tags editing, razor, nudge, items, transport, (protools-like)
