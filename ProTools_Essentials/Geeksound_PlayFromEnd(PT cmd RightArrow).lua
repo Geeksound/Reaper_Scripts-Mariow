fdc764ac505d39e40874efb806574ccaa25fb00e
Cédric Mariotti <cedricmariotti@gmail.com>
2026-03-14 10:05:57 +0100

    Update and rename PlayFromEnd(PT cmd RightArrow).lua to Geeksound_PlayFromEnd(PT cmd RightArrow).lua

--- ProTools_Essentials/Geeksound_PlayFromEnd(PT cmd RightArrow).lua
+++ ProTools_Essentials/Geeksound_PlayFromEnd(PT cmd RightArrow).lua
@@ -6,7 +6,7 @@
   v1.0 (2025-06-07)
   - Initial release
 @provides
-  [main] ProTools_Essentials/PlayFromEnd(PT cmd RightArrow).lua
+  [main] ProTools_Essentials/Geeksound_PlayFromEnd(PT cmd RightArrow).lua
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
 @tags editing, transport, playback, (protools-like)
