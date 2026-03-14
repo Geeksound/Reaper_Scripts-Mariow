ae2005d852a61c78be138dcf6e592965d4b8fa9a
Cédric Mariotti <cedricmariotti@gmail.com>
2026-03-14 09:52:08 +0100

    Update and rename ShrinkLeftEdge-TCui.lua to Geeksound_ShrinkLeftEdge-TCui.lua

--- ProTools_Essentials/Geeksound_ShrinkLeftEdge-TCui.lua
+++ ProTools_Essentials/Geeksound_ShrinkLeftEdge-TCui.lua
@@ -6,7 +6,7 @@
     v1.0 (2025-12-09)
     - Initial release (trims left edge of selected items using TimecodeUI nudge value)
 @provides
-    [main] ProTools_Essentials/ShrinkLeftEdge-TCui.lua
+    [main] ProTools_Essentials/Geeksound_ShrinkLeftEdge-TCui.lua
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
 @tags editing, trim, items, nudge, transport, (protools-like)
