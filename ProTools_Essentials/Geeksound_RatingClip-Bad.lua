0e11dc620cdcbd56a7f31a4ccb3a28f1db730435
Cédric Mariotti <cedricmariotti@gmail.com>
2026-03-14 09:48:59 +0100

    Update and rename RatingClip-Bad.lua to Geeksound_RatingClip-Bad.lua

--- ProTools_Essentials/Geeksound_RatingClip-Bad.lua
+++ ProTools_Essentials/Geeksound_RatingClip-Bad.lua
@@ -7,7 +7,7 @@
     - Initial release
     - Sets clip rating to Bad (1 star)
 @provides
-    [main] ProTools_Essentials/RatingClip-Bad.lua
+    [main] ProTools_Essentials/Geeksound_RatingClip-Bad.lua
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
 @tags rating, clip, protools-like, editing
