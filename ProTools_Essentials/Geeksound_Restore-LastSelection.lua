6d033d2d5db903a8caa7f0c0edc9da5a969aad9f
Cédric Mariotti <cedricmariotti@gmail.com>
2026-03-14 09:55:00 +0100

    Update and rename Restore-LastSelection.lua to Geeksound_Restore-LastSelection.lua

--- ProTools_Essentials/Geeksound_Restore-LastSelection.lua
+++ ProTools_Essentials/Geeksound_Restore-LastSelection.lua
@@ -6,7 +6,7 @@
   v1.0 (2025-06-07)
   - Initial release
 @provides
-  [main] ProTools_Essentials/RestoreLastSelection.lua
+  [main] ProTools_Essentials/Geeksound_RestoreLastSelection.lua
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
 @tags editing, items, timesel, watcher-dependency, (protools-like)
