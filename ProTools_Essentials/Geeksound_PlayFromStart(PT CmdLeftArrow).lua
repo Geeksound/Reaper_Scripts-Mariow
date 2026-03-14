a6aaa4f14520e326795e7c4879db9be9a5005bdb
Cédric Mariotti <cedricmariotti@gmail.com>
2026-03-14 10:05:14 +0100

    Update and rename PlayFromStart(PT CmdLeftArrow).lua to Geeksound_PlayFromStart(PT CmdLeftArrow).lua

--- ProTools_Essentials/Geeksound_PlayFromStart(PT CmdLeftArrow).lua
+++ ProTools_Essentials/Geeksound_PlayFromStart(PT CmdLeftArrow).lua
@@ -6,7 +6,7 @@
   v1.0 (2025-06-07)
   - Initial release
 @provides
-  [main] ProTools_Essentials/PlayFromStart(PT CmdLeftArrow).lua
+  [main] ProTools_Essentials/Geeksound_PlayFromStart(PT CmdLeftArrow).lua
 @link https://github.com/Geeksound/Reaper_Scripts-Mariow
 @repository https://github.com/Geeksound/Reaper_Scripts-Mariow
 @tags editing, transport, playback, (protools-like)
